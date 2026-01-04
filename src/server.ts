9aaeddf17fedcf995e6e8bd54c67698352442afc
sahariar8 <sahariaralam8@gmail.com>
2026-01-05 00:30:48 +0600

    project setup with prisma

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import { app } from "./app";
+import { prisma } from "./lib/prisma"
+
+
+const PORT = process.env.PORT || 3000;
+async function main() {
+    try {
+        await prisma.$connect();
+        console.log('connected to the database');
+        app.listen(PORT, () => {
+            console.log('Server is running on http://localhost:' + PORT);
+        });
+    } catch (er) {
+        console.log(er);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
