bb9992c6d80ef4948b50dc2fc65d5cf0bdb299dc
sahariar8 <sahariaralam8@gmail.com>
2026-01-06 18:06:48 +0600

    better-auth implementation

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-import express from 'express';
-import { postRouter } from './modules/post/post.router';
+import express from "express";
+import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
 
 app.use(express.json());
 
-
 // Sample route
-
-app.use('/posts', postRouter);
-app.get('/', (req, res) => {
-    res.send('Hello,Shahariar!');
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use("/posts", postRouter);
+app.get("/", (req, res) => {
+  res.send("Hello,Shahariar!");
 });
 
 export { app };
